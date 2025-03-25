89c210946624d4cbfa8544c0046e6dafbea6ad70
1tasnem <tasnemprodhan@gmail.com>
2025-03-26 00:50:36 +0600

    added video part

--- script/video.js
+++ script/video.js
@@ -15,6 +15,21 @@ console.log("load categories created");
 
 };
 
+
+
+
+const loadVideos = () =>
+    {
+        //fetch the data 
+    console.log("load categories created");
+        fetch ("https://openapi.programming-hero.com/api/phero-tube/videos")
+        .then((res) => res.json())
+        .then((data) => displayVideos(data.videos))
+        .catch ((error) => console.log(error));
+    
+    
+    };
+    
 //{ 
 // "category_id ": "1001",
 // "category" : "Music",
@@ -37,11 +52,42 @@ const displayCategories = (categories) =>
     //add button to caategory container
     categoryContainer.append(button);
    });
-
-
-
-        
-     
+    
 
     };
+
+
+    const displayVideos = (videos) => {
+       // console.log(videos);
+       const videoContainer = document.getElementById("videos");
+       videos.forEach((video) => 
+    {
+        console.log(video);
+        const card = document.createElement("div");
+        card.classList = "card bg-base-100  shadow-sm"
+        card.innerHTML = `<figure class="h-[200px]">
+    <img
+      src=${video.thumbnail}
+      class = "w-full h-full object-cover"
+      alt="Shoes" />
+  </figure>
+  <div class=" px-0 py-2 flex  gap-2">
+  <div >
+  <img class = "w-10 h-10 rounded-full object-cover" src=${video.authors[0].profile_picture}/>
+  </div>
+  <div >
+  <h2 class = "font-bold">${video.title}</h2>
+  <div class ="flex items-center gap-3">
+  
+  <p class = "text-gray-400">${video.authors[0].profile_name}</p>
+  <img class = "w-5" src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"
+  </div>
+  <p></p>
+ 
+  </div>
+  </div>`;
+  videoContainer.append(card);
+    })
+    }
 loadCategories();
+loadVideos();
